16da104ca5993ad510e84aa284ac0c2d3cb297fa
BAYUEQI <zouqi050813@gmail.com>
2025-06-17 11:27:32 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
                   </span>
                 </h1>
                 <p className="text-lg sm:text-xl text-gray-600 dark:text-gray-300 tracking-wide">
-                  临时邮箱服务
+                  支持qizou.dpdns.org,zouqimail.dpdns.org两种后缀
                 </p>
               </div>
 
